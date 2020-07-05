f8a478bf3d69fbdc0be4d5071e5b8fa88095a2ca
Max Alexandrov <max.alx@bk.ru>
2020-07-05 12:57:52 +0600

    Разделены дата и время в модели events.models.Event

--- events/models.py
+++ events/models.py
@@ -4,11 +4,10 @@ from django.db import models
 class Event(models.Model):
     user = models.ForeignKey('service_auth.CustomUser', on_delete=models.CASCADE, verbose_name='Пользователь')
     title = models.CharField(max_length=100, verbose_name='Заголовок')
-    datetime = models.DateTimeField(verbose_name='Дата и время события')
+    date = models.DateField(verbose_name='Дата события')
+    time = models.TimeField(verbose_name='Время события')
     tags = models.ManyToManyField('events.EventTag', blank=True, verbose_name='Теги')
     description = models.TextField(max_length=255, blank=True, verbose_name='Описание')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
 
     def __str__(self):
         return self.title
