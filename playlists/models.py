21ae8407634dc867daa9669cc1baec005a0ef26e
Max Alexandrov <max.alx@bk.ru>
2020-07-05 19:12:27 +0600

    Удалено ограничение на символы в плейлисте

--- playlists/models.py
+++ playlists/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Playlist(models.Model):
     user = models.OneToOneField('service_auth.CustomUser', on_delete=models.CASCADE, verbose_name='Пользователь')
-    link = models.TextField(max_length=200, verbose_name='Ссылка на плейлист или видео')
+    link = models.TextField(verbose_name='Ссылка на плейлист или видео')
 
     def __str__(self):
         return self.user.username
